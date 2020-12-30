409ea0362ce9e84704d3a06e605ff42aafc6e9c1
mZed83at <zeiti@gmx.at>
2020-12-30 21:07:08 +0100

    google Map eingebunden 4 Errors in MainActivity.kt

--- app/src/main/java/com/aichbauer/gpstracker_i/myActivity.kt
+++ app/src/main/java/com/aichbauer/gpstracker_i/myActivity.kt
@@ -1,4 +1,4 @@
 package com.aichbauer.gpstracker_i
 
 
-data class myActivity(val id: Long, val timestamp: Long, var title: String, var message: String, val latitude: Double, val longitude: Double)
+data class myActivity(val id: Long, val timestamp: Long, var title: String, var message: String, val latitude: Double?, val longitude: Double?)
